92c347cb462557c343feb3cb47de1ad4c3a44a64
DavisButler10 <45404502+DavisButler10@users.noreply.github.com>
2020-02-24 11:59:22 -0700

    Add files via upload

---
+++ FallingPlayerState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlayerState : IPlayerState
+{
+    Animator anim;
+
+    public void Enter(Player player)
+    {
+        player.mCurrentState = this;
+        anim = player.GetComponent<Animator>();
+        GameObject.FindGameObjectWithTag("Vegeta").GetComponent<Animator>().SetBool("Blast", true);
+        //GameObject.FindGameObjectWithTag("Blast").GetComponent<SpriteRenderer>().enabled = true;
+        Debug.Log(player.mCurrentState);
+    }
+
+    public void Execute(Player player)
+    {
+            IdlePlayerState idleState = new IdlePlayerState();
+            idleState.Enter(player);
+        
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public IPlayerState mCurrentState;
+
+    private void Awake()
+    {
+        mCurrentState = new SuperSaiyanIdlePlayerState();
+    }
+
+    void Update()
+    {
+        mCurrentState.Execute(this);
+    }
+}
